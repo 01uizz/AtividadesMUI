cfad3acbf8126224530ac3037e5faf8471f3498b
luizymanoel <0001150601@senaimgaluno.com.br>
2026-02-26 15:11:30 -0300

    segundo commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,25 +1,25 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import './index.css';
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
 
-import FormCadastro from './Components/FormCadastro';
-import Quantidade from './Components/Quantidade';
-import FormPreferencias from './Components/FormPreferencias';
-import FormConfiguracoes from './Components/FormConfiguracoes';
-import CadastrodeProdutos from './Components/CadastrodeProdutos';
-import ContadorEstoque from './Components/ContadorEstoque';
-import ExemploUseState from './Components/ExemploUseState';
+import FormCadastro from './Components/FormCadastro'
+import Quantidade from './Components/Quantidade'
+import FormPreferencias from './Components/FormPreferencias'
+import FormConfiguracoes from './Components/FormConfiguracoes'
+import CadastrodeProdutos from './Components/CadastrodeProdutos'
+import ContadorEstoque from './Components/ContadorEstoque'
+import ExemploUseState from './Components/ExemploUseState'
+import ExemploSweetAlert from './Components/ExemploSweetAlert'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <>
-      <FormCadastro />
-      <Quantidade />
-      <FormConfiguracoes />
-      <FormPreferencias />
-      <CadastrodeProdutos />
-      <ExemploUseState />
-      <ContadorEstoque />
-    </>
+    <FormCadastro />
+    <Quantidade />
+    <FormConfiguracoes />
+    <FormPreferencias />
+    <CadastrodeProdutos />
+    <ExemploUseState />
+    <ContadorEstoque />
+    <ExemploSweetAlert />
   </StrictMode>
-);
+)
